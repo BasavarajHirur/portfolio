2f3793624eb10db45342799e102cb0de9e7cd8cf
Basavaraja Hiruru <basavaraj.hirur746@gmail.com>
2025-07-29 20:36:17 +0530

    dynamic base url

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,1 +1,7 @@
-export const BASE_URL = '/api'; // change to relative url after app deployed ex: '/api'
+//This is for Production
+//export const BASE_URL = '/api';
+
+// This is for Development
+//export const BASE_URL = 'http://localhost:3000';
+
+export const BASE_URL = location.hostname === 'localhost' ? 'http://localhost:7777' : '/api';
